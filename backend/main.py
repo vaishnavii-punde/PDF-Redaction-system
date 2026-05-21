cfd2b6708be0d022aa7d59a7be7b3d2ae067e682
vaishnavii-punde <vaishnavii.punde14@gmail.com>
2026-05-21 15:13:10 +0530

    Improvement commit

--- backend/main.py
+++ backend/main.py
@@ -259,5 +259,5 @@ def get_audit_log():
 
 if __name__ == '__main__':
     import uvicorn
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 10000))
     uvicorn.run('main:app', host='0.0.0.0', port=port)
